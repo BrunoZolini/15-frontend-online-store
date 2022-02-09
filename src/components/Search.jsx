50da1d9c34833d1b04e8bc5ceb6654a12caae507
Laura Lima <lauraolima2000@gmail.com>
2022-02-09 20:34:50 -0300

    tentando dar git pull terceira vez

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class Search extends Component {
   render() {
@@ -10,6 +11,19 @@ class Search extends Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        {
+          getCategories().map(({ name, id }) => (
+            <section
+              key={ id }
+            >
+              <button
+                type="button"
+                data-testid="category"
+              >
+                { console.log(name) }
+              </button>
+            </section>))
+        }
       </div>
     );
   }
