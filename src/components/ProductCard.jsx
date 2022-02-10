edd019fee29be007bc6c3f28170c90e51be3533f
Bruno Zolini <bruno.zolini@hotmail.com>
2022-02-10 16:02:24 -0300

    Requisito 5

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ProductCard extends Component {
+  render() {
+    const {
+      title,
+      thumbnail,
+      price,
+    } = this.props;
+    return (
+      <div data-testid="product">
+        <p>{ title }</p>
+        <img src={ thumbnail } alt={ title } />
+        <p>{ price }</p>
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+export default ProductCard;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
+import ProductsList from './ProductsList';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '',
+      isButtonClicked: false,
+      searchValue: '',
+    };
+  }
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleButton = () => {
+    const { inputValue } = this.state;
+    this.setState({ isButtonClicked: true, searchValue: inputValue });
+  }
+
   render() {
+    const { inputValue, isButtonClicked, searchValue } = this.state;
     return (
       <div>
         <form>
-          <input type="text" />
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.handleButton }
+          >
+            Pesquisar
+          </button>
+          <input
+            data-testid="query-input"
+            name="inputValue"
+            type="text"
+            value={ inputValue }
+            onChange={ this.handleChange }
+          />
         </form>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        { !isButtonClicked ? (
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>) : <ProductsList searchValue={ searchValue } /> }
+
       </div>
     );
   }
