34c404f8d91830d976f8ce3b04d1cf0d38f8eda4
Kako <joyceandronico@msn.com>
2022-02-10 13:46:37 -0300

    Requisito 3

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import cart from '../icons/shopping-cart.png';
 
 class Search extends Component {
   render() {
@@ -10,6 +12,8 @@ class Search extends Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link data-testid="shopping-cart-button" to="/CartButton" />
+        <img src={ cart } alt="shopping-cart-icon" />
       </div>
     );
   }
