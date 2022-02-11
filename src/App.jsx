30c73b86a6ae35c1ccba69365880aa0c44297c81
“carvalhoMateus” <mateus.devweb@gmail.com>
2022-02-11 20:38:39 -0300

    Requisito 8 - incompleto

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
 
+// coments
+
 class App extends Component {
   render() {
     return (
