cccccde7dec7401d2cc9be1d9671161d65331f6e
Kako <joyceandronico@msn.com>
2022-02-14 20:01:13 -0300

    Requisito 9 incompleto2

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import Search from './components/Search';
 import ProductDetails from './components/ProductDetails';
 
 class App extends Component {
+  const addTocart
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={ Search } />
+          <Route exact path="/" component={ Search } render={(...props)=><Search {...props} addTocar={this.addtocart} func/>} />
           <Route path="/product-details/:id" component={ ProductDetails } />
         </Switch>
         {/* // teste */}
