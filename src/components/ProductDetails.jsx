ae756fa720c9977237fe5d4aecac0234eebd9e63
Laura Lima <lauraolima2000@gmail.com>
2022-02-14 15:58:00 -0300

    commit do eduardo

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,43 +1,43 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import { getProductFromId } from '../services/api';
+import PropTypes from 'prop-types';
+import { getProductFromId } from '../services/api';
 
 class ProductDetails extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     productName: '',
-  //     productImage: '',
-  //   };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      productName: '',
+      productImage: '',
+    };
+  }
 
-  // async componentDidMount() {
-  //   const { match: { params: { id } } } = this.props;
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
 
-  //   const productReturn = await getProductFromId(id);
-  //   this.setState({
-  //     productName: productReturn.title,
-  //     productImage: productReturn.thumbnail,
-  //   });
-  // }
+    const productReturn = await getProductFromId(id);
+    this.setState({
+      productName: productReturn.title,
+      productImage: productReturn.thumbnail,
+    });
+  }
 
   render() {
-    // const { productName, productImage } = this.state;
+    const { productName, productImage } = this.state;
     return (
       <div>
-        {/* <p>{ productName }</p>
-        <img src={ productImage } alt={ productName } /> */}
+        {/* {/* <p>{ productName }</p> */}
+        <img src={ productImage } alt={ productName } />
         <div>oi</div>
       </div>
     );
   }
 }
 
-// ProductDetails.propTypes = {
-//   match: PropTypes.shape({
-//     params: PropTypes.objectOf(PropTypes.string),
-//     id: PropTypes.string,
-//   }).isRequired,
-// };
+ProductDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.objectOf(PropTypes.string),
+    id: PropTypes.string,
+  }).isRequired,
+};
 
 export default ProductDetails;
